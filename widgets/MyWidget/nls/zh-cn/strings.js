1ffb8c5dddadf2a42c7541d87598f68583a51cc5
yusuffadhlurahman <yusuffadil19@gmail.com>
2021-11-03 10:46:27 +0700

    initial commit

---
+++ widgets/MyWidget/nls/zh-cn/strings.js
@@ -0,0 +1,4 @@
+﻿define({
+    label1 : "我的widget。",
+    label2 : "这里可以配置。"
+});
---
+++ widgets/samplewidgets/MyWidget/nls/strings.js
@@ -0,0 +1,7 @@
+﻿define({
+    root:{
+        label1: "This is my widget.",
+        label2: "This is configurable."
+    },
+    "zh-cn": true    
+});
